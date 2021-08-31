758392058b6a61b51d44823b959cd26fe4cedde6
NeviVen <58914442+NeviVen@users.noreply.github.com>
2021-08-31 15:50:31 +0300

    Add files via upload

---
+++ server/constants/Constants.js
@@ -0,0 +1,16 @@
+
+
+const SERVER_PORT = 8000;
+const REFRESH_RATE = 25;
+
+const X_STARTING_POS = 100;
+const Y_STARTING_POS = 100;
+const PLAYER_SPEED = 10;
+const STARTING_DIR = 'down';
+const MONGO_REPO = "Account";
+
+const RPS = {
+    PAPER: "Paper",
+    SCISSOR: "Scissors",
+    ROCK: "Rock"
+};
